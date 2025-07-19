2fa9a042f61ca0bf13f0b903844a23a90020c1fd
nobrainghost <mulwabenard24@gmail.com>
2025-07-19 16:13:18 +0300

    better fanning

--- internal/fan.go
+++ internal/fan.go
@@ -1,14 +1,18 @@
 package internal
 
-//This is a manual broadcastor/fanner -- fans out
-//reads from parsed log channel and then distributes this to the channels that need
-// to remove competition from the parsed log channel
+import "log"
 
 func (a *App) FanOut() {
 	for l := range a.ParsedLogChan {
-		a.MetricChan <- l
-		a.DbRawWriteChan <- l
-
+		select {
+		case a.MetricChan <- l:
+		default:
+			log.Printf("MetricChan full, dropping log from %s", l.SourceDyno)
+		}
+		select {
+		case a.DbRawWriteChan <- l:
+		default:
+			log.Printf("DbRawWriteChan full, dropping log from %s", l.SourceDyno)
+		}
 	}
-
 }
